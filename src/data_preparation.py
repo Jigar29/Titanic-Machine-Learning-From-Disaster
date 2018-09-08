4c54be02be3fb42a4bff5957fca6c210c5520909
Jigar <jigaragrawal29@gmail.com>
2018-09-08 03:55:52 -0700

    Reading from pandas completed, yet to work on the tensorflow part

---
+++ src/data_preparation.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+import pandas as pd
+
+class Datapreparation(object):
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.dataframe = pd.DataFrame()
+        return
+
+    '''Fuction to create the pandas dataframe from CSV File'''
+    def createDataFrameFromCSVFile(self):
+        if (self.dataframe.empty):
+            self.dataframe = pd.read_csv(self.filepath)
+        else:
+            print("The DataFrame is already created")
+
+        return
+
+    '''Fuction to create the pandas dataframe from Excel File'''
+    def createDataFrameFromExcelFile(self):
+        if(self.dataframe.empty):
+            self.dataframe = pd.read_excel(self.filepath)
+        else:
+            print("The DataFrame is already created")
+
+        return
+
+    '''Fuction to create the tensor object from Excel File'''
+    def createTensorObjectFromCSVFile(self):
+        self.tensor = tf.read_file(self.filepath, "Data_Tensor")
+        return
+
+    def createTensorObjectFromExcelFile(self):
+        self.tensor = tf.read_file(self.filepath, "Data_Tensor")
+        return
+
+train_data_obj = Datapreparation("../data/train.csv")
+train_data_obj.createDataFrameFromCSVFile()
+train_data_obj.createDataFrameFromCSVFile()
+test_data_obj  = Datapreparation("../data/test.csv")
