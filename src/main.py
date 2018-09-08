e4bc1f7804ca35b983c530b8433fc19cca7f1095
Jigar <jigaragrawal29@gmail.com>
2018-09-08 15:27:50 -0700

    Titanic_data_preparation separated from DataPreparation

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-from src.data_preparation import Datapreparation, Titanic
+from src.legacy.data_preparation import Datapreparation
+from src.titanic_data_preparation import Titanic
 
 if __name__ == "__main__":
     titanic_obejct = Titanic.getInstance("../data/train.csv", "../data/test.csv")
